164ca4a6d378bb86b7196968176e40271ec33efc
Mike Gratton <michael.b.gratton@gmail.com>
2023-03-10 20:36:07 -0500

    Fix build issues

--- cmake/IdlTarget.cmake
+++ cmake/IdlTarget.cmake
@@ -1,10 +1,9 @@
 ###########################################################################
 # IdlTarget -- make a library target whose source comes from compiling idls
 #    
-# IdlTarget([SHARED] [PATH path] INCLUDE ... SOURCE ...)
+# IdlTarget([PATH path] INCLUDE ... SOURCE ...)
 #
 # Arguments:
-#    SHARED -- build a shared library (default is static library)
 #    PATH -- Place the h, cxx files in "path," include for foo.idl will be "path/foo.h"
 #    INCLUDE -- list of other directories to include while compiling idls
 #    SOURCE -- list of idl files 
@@ -21,6 +20,7 @@
 #   idl_header -- h files from compilation
 #   idl_source -- cxx files from compilation
 macro(CompileIdl )
+	# cmake_policy(SET CMP0145 OLD)
    cmake_parse_arguments(idl "" "PATH" "INCLUDE;SOURCE" ${ARGN})
    foreach( incl ${idl_INCLUDE})
        list(APPEND ddsgen_include -I ${incl})
@@ -36,14 +36,22 @@ macro(CompileIdl )
    foreach(idl ${idl_SOURCE})
        get_filename_component(stem ${idl} NAME_WE)
        get_filename_component(idl_abs ${idl} ABSOLUTE BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+       if (CMAKE_GENERATOR STREQUAL "Ninja")
        add_custom_command(OUTPUT ${idl_ABS_PATH}/${stem}.h ${idl_ABS_PATH}/${stem}.cxx
             COMMAND cpp ${ddsgen_include} -MM ${idl_abs} -MF ${idl_ABS_PATH}/${stem}.idl.d
             COMMAND sed -i -e "s,${stem}.o,${relpath}/${stem}.h ${relpath}/${stem}.cxx," ${idl_ABS_PATH}/${stem}.idl.d
-            COMMAND ${ddsgen} -cs -replace ${ddsgen_include} ${idl_abs}
+	    COMMAND ${ddsgen} -d ${idl_ABS_PATH}  -cs -replace ${ddsgen_include} ${idl_abs}
             DEPFILE ${idl_ABS_PATH}/${stem}.idl.d
             DEPENDS ${idl_abs} 
             COMMENT "Compiling idl ${idl_abs}"
             )
+   	else()
+	add_custom_command(OUTPUT ${idl_ABS_PATH}/${stem}.h ${idl_ABS_PATH}/${stem}.cxx
+            COMMAND ${ddsgen} -cs -replace ${ddsgen_include} ${idl_abs}
+            DEPENDS ${idl_abs} 
+            COMMENT "Compiling idl ${idl_abs}"
+	    )
+       	endif()
        list(APPEND idl_source ${idl_ABS_PATH}/${stem}.cxx)
    endforeach()
 endmacro()
@@ -51,12 +59,8 @@ endmacro()
 ###############################################################################
 macro(IdlTarget name)
    cmake_parse_arguments(idl "SHARED" "" "" ${ARGN})
-   CompileIdl(${idl_UNPARSED_ARGUMENTS})
-   if (idl_SHARED)
-       add_library(${name} SHARED ${idl_source})
-   else()
-       add_library(${name} STATIC ${idl_source})
-   endif()
+   CompileIdl(${idl_UNPARSED_ARGUMENTS})   
+   add_library(${name} ${idl_source})   
    target_link_libraries(${name} PUBLIC fastrtps)
    target_include_directories(${name} 
         PUBLIC 
